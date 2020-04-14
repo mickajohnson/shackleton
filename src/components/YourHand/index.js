ee9e2a11056f5227e930c1d7c5bc5236716cf0b0
Mick Johnson <mick.johnson@homechef.com>
2020-04-13 21:05:19 -0500

    making hand look better

--- src/components/YourHand/index.js
+++ src/components/YourHand/index.js
@@ -4,12 +4,17 @@ import './YourHand.css';
 import PlayerContext from '../../context/PlayerContext';
 import HandCard from '../HandCard';
 
-const getRotation = (cards, cardIndex) => {
+const getDiffFromMiddle = (cards, cardIndex) => {
   const middle = cards.length / 2;
-  return (cardIndex - middle) * 2;
+  return cardIndex - middle;
 };
 
-const getTranslation = (cards, cardIndex) => Math.abs(getRotation(cards, cardIndex)) * 3;
+const getRotation = (cards, cardIndex) => {
+  return getDiffFromMiddle(cards, cardIndex) * 4;
+};
+
+const getTranslation = (cards, cardIndex) =>
+  Math.pow(Math.abs(getDiffFromMiddle(cards, cardIndex)), 2);
 
 const getSeparation = (cards) => (cards.length > 15 ? 15 : cards.length);
 
@@ -40,7 +45,7 @@ const GameScreen = () => {
                 transform: `rotateZ(${getRotation(hand, idx)}deg) translate(0px,${getTranslation(
                   hand,
                   idx
-                )}px)`,
+                )}%)`,
                 margin: `0 -${getSeparation(hand)}px`,
               }}
             />
